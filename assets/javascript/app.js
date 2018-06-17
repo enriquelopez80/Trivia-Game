509c1d2d508dfe1b6e6fa3b4437e9fd006e32595
Rico <enriquelopez80@gmail.com>
2018-06-16 17:09:14 -0700

    clear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,18 +8,18 @@ $(document).ready(function () {
 
     function results() {
 
-        let q1 = $('input[type=radio][name="q1"]:checked').val();
-        let q2 = $('input[type=radio][name="q2"]:checked').val();
-        let q3 = $('input[type=radio][name="q3"]:checked').val();
-        let q4 = $('input[type=radio][name="q4"]:checked').val();
-        let q5 = $('input[type=radio][name="q5"]:checked').val();
-        let q6 = $('input[type=radio][name="q6"]:checked').val();
-        let q7 = $('input[type=radio][name="q7"]:checked').val();
-        let q8 = $('input[type=radio][name="q8"]:checked').val();
+        let q1 = $('input[name="q1"]:checked').val();
+        let q2 = $('input[name="q2"]:checked').val();
+        let q3 = $('input[name="q3"]:checked').val();
+        let q4 = $('input[name="q4"]:checked').val();
+        let q5 = $('input[name="q5"]:checked').val();
+        let q6 = $('input[name="q6"]:checked').val();
+        let q7 = $('input[name="q7"]:checked').val();
+        let q8 = $('input[name="q8"]:checked').val();
 
         if (q1 === 'a') {
             correct++;
-        } else if (q1 === undefined) {
+        } else if (q1 === '') {
             unanswered++;
         } else {
             wrong++;
@@ -27,7 +27,7 @@ $(document).ready(function () {
 
         if (q2 === 'c') {
             correct++;
-        } else if (q2 === undefined) {
+        } else if (q2 === '') {
             unanswered++;
         } else {
             wrong++;
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
         if (q3 === 'c') {
             correct++;
-        } else if (q3 === undefined) {
+        } else if (q3 === '') {
             unanswered++;
         } else {
             wrong++;
@@ -43,7 +43,7 @@ $(document).ready(function () {
 
         if (q4 === 'c') {
             correct++;
-        } else if (q4 === undefined) {
+        } else if (q4 === '') {
             unanswered++;
         } else {
             wrong++;
@@ -51,7 +51,7 @@ $(document).ready(function () {
 
         if (q5 === 'b') {
             correct++;
-        } else if (q5 === undefined) {
+        } else if (q5 === '') {
             unanswered++;
         } else {
             wrong++;
@@ -59,7 +59,7 @@ $(document).ready(function () {
 
         if (q6 === 'a') {
             correct++;
-        } else if (q6 === undefined) {
+        } else if (q6 === '') {
             unanswered++;
         } else {
             wrong++;
@@ -67,7 +67,7 @@ $(document).ready(function () {
 
         if (q7 === 'c') {
             correct++;
-        } else if (q7 === undefined) {
+        } else if (q7 === '') {
             unanswered++;
         } else {
             wrong++;
@@ -75,7 +75,7 @@ $(document).ready(function () {
 
         if (q8 === 'd') {
             correct++;
-        } else if (q8 === undefined) {
+        } else if (q8 === '') {
             unanswered++;
         } else {
             wrong++;
